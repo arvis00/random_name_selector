9d01814fa65a6fbef13eb781fae112923dacd443
Arvydas_Mickus <amickus@icloud.com>
2020-04-18 12:02:35 +0300

    build folder

---
+++ build/precache-manifest.4603dead6bc5b88fd6073e4668a0abdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198ddc777918acc8cb80ca033a48610c",
+    "url": "/random_name_selector/index.html"
+  },
+  {
+    "revision": "146d80974ea97495f53e",
+    "url": "/random_name_selector/static/css/main.3cfa60e6.chunk.css"
+  },
+  {
+    "revision": "facc39cc536b9c06af14",
+    "url": "/random_name_selector/static/js/2.2ba3dc93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random_name_selector/static/js/2.2ba3dc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146d80974ea97495f53e",
+    "url": "/random_name_selector/static/js/main.23ed72f9.chunk.js"
+  },
+  {
+    "revision": "ec10d6b88c10df512026",
+    "url": "/random_name_selector/static/js/runtime-main.2f0ddbaa.js"
+  }
+]);
